d8d0a0ddd5cd1f710bb29a2a543fbd19fb1028d7
omar <omarjemal@cmail.carleton.ca>
2019-10-10 16:58:34 -0400

    add intent feature Lab 5

---
+++ Lab 5/app/src/main/java/com/example/myapplication4/Main2Activity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView text = (TextView) findViewById(R.id.textView);
+        EditText input = (EditText) findViewById(R.id.editText2);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+
+}
